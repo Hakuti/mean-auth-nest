6b1e2c071ee3d7851675c945ea9f6130975c9f07
Jean Canales <jeancanales@Jeans-MacBook-Pro.local>
2019-04-18 10:21:43 -0400

    Added Contact DTO, Contact Interface, and Contact Model

---
+++ src/contacts/dto/create-contact.dto.ts
@@ -0,0 +1,7 @@
+export class CreateContactDto {
+    readonly firstName: string;
+    readonly lastName: string;
+    readonly email: string;
+    readonly company: string;
+    readonly phone: number;
+}
---
+++ src/contacts/interfaces/contacts.interface.ts
@@ -0,0 +1,7 @@
+export interface Contact {
+    firstName: string;
+    lastName: string;
+    email: string;
+    company: string;
+    phone: number;
+}
